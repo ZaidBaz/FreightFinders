f943dc585816c8da7c7fd0646fa39656a99eca82
Zalissa <zongokafando@wisc.edu>
2024-11-30 11:49:01 -0600

    horizontal scrolling, dropdown list and autofill suggestions fixed

--- frontend/src/Components/Dropdown/Dropdown.js
+++ frontend/src/Components/Dropdown/Dropdown.js
@@ -1,31 +1,17 @@
 import React, { useState } from 'react';
 import './Dropdown.css';
 
-const Dropdown = ({setSelectedCapacity,selectedCapacity}) => {
+const Dropdown = ({ setSelectedCapacity, selectedCapacity }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedOptions, setSelectedOptions] = useState({
-    dray: false,
-    dryVan: false,
-    refrigerated:false,
-    powerOnly: false,
-    speciality: false,
-  });
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
 
-  // const handleCheckboxChange = (option) => {
-  //   setSelectedOptions(prevState => ({
-  //     ...prevState,
-  //     [option]: !prevState[option],
-  //   }));
-  // };
-
   const handleCapacityChange = (event) => {
     const value = event.target.value;
     setSelectedCapacity((prev) =>
-      prev.includes(value) ? prev.filter((v) => v !== value) : [...prev, value] // Corrected parentheses here
+      prev.includes(value) ? prev.filter((v) => v !== value) : [...prev, value]
     );
   };
 
@@ -36,21 +22,21 @@ const Dropdown = ({setSelectedCapacity,selectedCapacity}) => {
         <span className="dropdown-arrow">{isOpen ? '▲' : '▼'}</span>
       </div>
       {isOpen && (
-                          <div className="dropdown-content">
-                            {['power only', 'dry van', 'dray', 'refrigerated', 'specialty'].map((capacity) => (
-                              <div key={capacity} className="checkbox-item">
-                                <input
-                                  type="checkbox"
-                                  id={capacity}
-                                  value={capacity}
-                                  onChange={handleCapacityChange}
-                                  checked={selectedCapacity.includes(capacity)}
-                                />
-                                <label htmlFor={capacity}>{capacity.replace('-', ' ').toUpperCase()}</label>
-                              </div>
-                            ))}
-                          </div>
-                        )}
+        <div className="dropdown-content">
+          {['power only', 'dry van', 'dray', 'refrigerated', 'specialty'].map((capacity) => (
+            <div key={capacity} className="checkbox-item">
+              <input
+                type="checkbox"
+                id={capacity}
+                value={capacity}
+                onChange={handleCapacityChange}
+                checked={selectedCapacity.includes(capacity)}
+              />
+              <label htmlFor={capacity}>{capacity.replace('-', ' ').toUpperCase()}</label>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
